042ecce3439272cf2e1bddca356dee92e6916359
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-06 22:12:45 -0300

    feat: estruturando pacote e modulos

---
+++ restaurant/management.py
@@ -0,0 +1,10 @@
+from menu import AVAILABLE_MENU
+
+# def get_item(item_id):
+
+
+# def add_item_to_tab(table_tab, item_id, amount):
+
+
+# def calculate_tab(table_tab):
+
---
+++ restaurant/screen.py
@@ -0,0 +1,7 @@
+TABLE_TAB = []
+
+# def initial_screen():
+
+# def add_item_screen():
+
+# def check_out_screen():
