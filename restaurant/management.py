00b309aaa2a10a330904fdc4231c7020575aef31
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-06 22:42:49 -0300

    feat: função get_item

--- restaurant/management.py
+++ restaurant/management.py
@@ -1,9 +1,14 @@
 from menu import AVAILABLE_MENU
 
-# def get_item(item_id):
+# print(AVAILABLE_MENU)
 
+def get_item(item_id: int):
+    for produto in AVAILABLE_MENU:
+        if produto['id'] == item_id:
+            return produto
 
 # def add_item_to_tab(table_tab, item_id, amount):
+
 
 
 # def calculate_tab(table_tab):
