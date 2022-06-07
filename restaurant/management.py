5208e8204d30488e8aaf4472cbd90eabfe27858c
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-07 15:21:56 -0300

    feat:funcoes initial_screen e add_item_screen

--- restaurant/management.py
+++ restaurant/management.py
@@ -1,6 +1,4 @@
-from menu import AVAILABLE_MENU
-
-# print(AVAILABLE_MENU)
+from restaurant.menu import AVAILABLE_MENU
 
 def get_item(item_id: int):
     for produto in AVAILABLE_MENU:
@@ -14,18 +12,10 @@ def add_item_to_tab(table_tab, item_id, amount):
     else:
         produto['amount'] = amount
         table_tab.append(produto)
-        print(table_tab)
         return True
-
-add_item_to_tab([ {"id": 34, "name": "FRENCH TOAST", "price": 5.0}], 1, 2)
 
 def calculate_tab(table_tab):
     valor_total = 0
     for produto in table_tab:
         valor_total+= produto['price']*produto['amount']
     return valor_total
-    
-calculate_tab([{'id': 1, 'name': 'ADICIONAL FRANGO 50G', 'price': 4.0, 'amount': 3},
- {"id": 34, "name": "FRENCH TOAST", "price": 5.0, 'amount': 2},
-  {"id": 32, "name": "COOKIE", "price": 5.0, 'amount': 1}
-])
--- restaurant/screen.py
+++ restaurant/screen.py
@@ -1,9 +1,39 @@
-from management import add_item_to_tab
+import os
+from restaurant.management import add_item_to_tab
 
 TABLE_TAB = []
 
-# def initial_screen():
+def initial_screen():
+    while True:
+        print('1. Adicionar item a comanda')
+        print('2. Fechar comanda')
+        resposta = input('Digite o que deseja fazer: ')
+        os.system('clear')
+        if resposta == '1':
+            add_item_screen()
+        elif resposta == '2':
+            check_out_screen()
+        else:
+            print('Digite uma opção válida (1 ou 2)')
 
-# def add_item_screen():
 
-# def check_out_screen():
+
+def add_item_screen():
+    while True:
+        id_do_item = int(input('Digite o id do item:'))
+        quantidade_do_item = int(input('Digite a quantidade desejada:'))
+        adicionar_produtos = add_item_to_tab(TABLE_TAB, id_do_item, quantidade_do_item)
+        os.system('clear')
+        if adicionar_produtos == False:
+            print(f'{id_do_item} não é um id de item válido')
+        else:
+            for item in TABLE_TAB:
+                nome_do_item = item['name']
+            print(f'{quantidade_do_item} {nome_do_item} adicionado(s) a comanda!')
+            print('\n')
+
+            break
+        initial_screen()
+
+
+def check_out_screen():
