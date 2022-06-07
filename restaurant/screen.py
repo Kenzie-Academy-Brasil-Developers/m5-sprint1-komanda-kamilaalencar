fb2ee5f59ebd1208980c3efd62f1e1e647259849
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-07 16:22:36 -0300

    feat: funcao check_out_screen

--- restaurant/screen.py
+++ restaurant/screen.py
@@ -1,5 +1,5 @@
 import os
-from restaurant.management import add_item_to_tab
+from restaurant.management import add_item_to_tab, calculate_tab
 
 TABLE_TAB = []
 
@@ -14,7 +14,8 @@ def initial_screen():
         elif resposta == '2':
             check_out_screen()
         else:
-            print('Digite uma opção válida (1 ou 2)')
+            print('Digite uma opção válida (1 ou 2)\n')
+        break
 
 
 
@@ -25,15 +26,30 @@ def add_item_screen():
         adicionar_produtos = add_item_to_tab(TABLE_TAB, id_do_item, quantidade_do_item)
         os.system('clear')
         if adicionar_produtos == False:
-            print(f'{id_do_item} não é um id de item válido')
+            print(f'{id_do_item} não é um id de item válido\n')
         else:
             for item in TABLE_TAB:
                 nome_do_item = item['name']
-            print(f'{quantidade_do_item} {nome_do_item} adicionado(s) a comanda!')
-            print('\n')
-
+            print(f'{quantidade_do_item} {nome_do_item} adicionado(s) a comanda!\n')
             break
-        initial_screen()
+    initial_screen()
 
 
 def check_out_screen():
+    while True:
+        os.system('clear')
+        for indice, produto in enumerate(TABLE_TAB):
+            numero_do_item = indice + 1
+            quantidade = produto['amount']
+            nome = produto['name']
+            sub_total = produto['price'] * quantidade
+            print(f'Item {numero_do_item} : {quantidade} {nome} - R${sub_total}')
+        print('-' * 100)
+        print(f'Total: R${calculate_tab(TABLE_TAB)}\n')
+        print('Digite F para finalizar o sistema')
+        fechamento = input()
+        if fechamento.upper() == 'F':
+            break
+
+
+
